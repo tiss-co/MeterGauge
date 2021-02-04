5442a70f754c128de8b4c69c575c7bb3897de7ed
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2021-02-04 16:03:37 +0330

    Initial Commit

---
+++ MeterGauge/MeterGauge.h
@@ -0,0 +1,18 @@
+//
+//  MeterGauge.h
+//  MeterGauge
+//
+//  Created by Amir on 2/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MeterGauge.
+FOUNDATION_EXPORT double MeterGaugeVersionNumber;
+
+//! Project version string for MeterGauge.
+FOUNDATION_EXPORT const unsigned char MeterGaugeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeterGauge/PublicHeader.h>
+
+

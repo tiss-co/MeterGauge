92e80da63bf46bdcd0b2a48f45ea20255ff233c6
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2021-02-11 16:59:51 +0330

    Initial library

---
+++ MeterGauge/MeterGauge.swift
@@ -0,0 +1,20 @@
+//
+//  MeterGauge.swift
+//  MeterGauge
+//
+//  Created by Amir on 2/4/21.
+//
+
+import UIKit
+
+class MeterGauge: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MeterGaugeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeterGaugeExample
+//
+//  Created by Amir on 2/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
